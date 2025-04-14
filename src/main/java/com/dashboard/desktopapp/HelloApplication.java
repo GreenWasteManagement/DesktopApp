dc2fcaa7d0bb2c100aa6402d250a17079f66b9e5
André Costa <andre.costa@ipvc.pt>
2025-04-14 10:54:26 +0100

    Icon and stylesheet imports solved, this was not so good of a start

--- src/main/java/com/dashboard/desktopapp/HelloApplication.java
+++ src/main/java/com/dashboard/desktopapp/HelloApplication.java
@@ -7,12 +7,23 @@ import javafx.stage.Stage;
 import javafx.scene.image.Image;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+
+        // Add CSS stylesheet
+        String css = this.getClass().getResource("style.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        // Set app icon
+        Image image = new Image(this.getClass().getResourceAsStream("images/APP_LOGO.png"));
+        stage.getIcons().add(image);
+
         stage.setTitle("Viana Circular");
         stage.setScene(scene);
         stage.show();
