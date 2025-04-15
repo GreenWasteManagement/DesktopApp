c47202048b67b3dbf2f7559b97889ce581416293
André Costa <andre.costa@ipvc.pt>
2025-04-15 11:38:00 +0100

    Menu card action test template function, homepage frontend done

--- src/main/java/com/dashboard/desktopapp/HomeNavController.java
+++ src/main/java/com/dashboard/desktopapp/HomeNavController.java
@@ -1,4 +1,11 @@
 package com.dashboard.desktopapp;
 
+import javafx.fxml.FXML;
+
 public class HomeNavController {
+
+    @FXML
+    public void Test(javafx.scene.input.MouseEvent event) {
+        System.out.println("Hello World");
+    }
 }
